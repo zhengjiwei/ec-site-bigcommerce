02ce927b81dddf023f77bffe78c2bc5bc859d8af
zheng-jiwei <zheng.jwei@connecty.co.jp>
2020-03-05 10:08:09 +0900

    modify port and delete some files

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [org.clojure/data.json "0.2.6"]
-   [clj-http "3.10.0"]
-   [com.auth0/java-jwt "3.8.2"]
-	[cheshire "5.7.0"]
+			   [clj-http "3.10.0"]
+			   [com.auth0/java-jwt "3.8.2"]
+				[cheshire "5.7.0"]
                  ]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler ecsite.handler/app :port 3030}
+  :ring {:handler ecsite.handler/app :port 8080}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
---
+++ readme.md
@@ -0,0 +1,2 @@
+# １、lein ring server-headless でサーバーを起動してください。
+# ２、ブラザーで http://localhost:8080/index.html をアクセスしたら、サイトの表示ができます。
