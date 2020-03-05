6d1f27aa14b1cf0fa27d746cd207d6783cb68a59
zheng-jiwei <zheng.jwei@connecty.co.jp>
2020-03-05 11:15:19 +0900

    change reading image file

--- src/ecsite/handler.clj
+++ src/ecsite/handler.clj
@@ -6,6 +6,8 @@
       [clojure.data.json :as json]
       [clojure.edn :as edn]
       [ecsite.storefront :as storefront]
+      [clojure.java.io :as io]
+      [ring.util.response :as response]
       )
     )
 
@@ -14,19 +16,7 @@
 ;{:params (get-in request [:params :sql])}
 
 (defroutes app-routes
-           (GET "/*" request
-                 (let [uri (:uri request)]
-                 (prn "### uri=" uri)
-                      {
-                       :status 200
-                       :headers {
-                            "Access-Control-Allow-Headers" "Origin, X-Requested-With, Content-Type, Accept",
-                           "Access-Control-Allow-Origin" "*"
-                                 }
-                       :body (slurp (str "resources" uri))
-                       }
-                      )
-                 )
+      (GET "/*" request (response/response (io/file (str "resources" (:uri request)))))
 		  (POST "/bigcommerce/graphql/query" request (storefront/process-graphql request))
 		  (POST "/bigcommerce/front/query" request (storefront/process-front request))
 		  (POST "/bigcommerce/server/query" request (storefront/process-server request))
